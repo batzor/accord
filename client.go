bc9efb6e7a5e354da4fffcbf4a2644e75e90df28
qvntm <32958247+qvntm@users.noreply.github.com>
2020-08-11 17:23:05 +0900

    Implement user authentication

--- client.go
+++ client.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	pb "github.com/qvntm/Accord/pb"
 )
@@ -14,19 +18,79 @@ type AccordClient struct {
 	Token string
 }
 
-func (c *AccordClient) Start() {
+func NewAccordClient() *AccordClient {
+	return &AccordClient{}
+
+}
+
+func (cli *AccordClient) Start() {
 	fmt.Println("Starting up!")
-	conn, err := grpc.Dial("localhost:12345", grpc.WithInsecure())
+	conn, err := grpc.Dial("0.0.0.0:12345", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to init listener: %v", err)
 	}
-	defer conn.Close()
 
-	c.ChatClient = pb.NewChatClient(conn)
+	cli.ChatClient = pb.NewChatClient(conn)
 	fmt.Println("Successfully started!")
+}
+
+// CreateLaptop calls create laptop RPC
+func (cli *AccordClient) CreateUser(username string, password string) {
+	req := &pb.CreateUserRequest{
+		Username: username,
+		Password: password,
+	}
+
+	log.Print("Creating user...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := cli.ChatClient.CreateUser(ctx, req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.AlreadyExists {
+			log.Print("Username already exists")
+		} else if ok && st.Code() == codes.InvalidArgument {
+			log.Print("Invalid password")
+		} else {
+			log.Fatalf("Unexpected error: %v", err)
+		}
+		return
+
+	}
+
+	log.Printf("created user: %s", res.Token)
+}
+
+func (cli *AccordClient) Login(username string, password string) {
+	req := &pb.LoginRequest{
+		Username: username,
+		Password: password,
+	}
+
+	log.Print("Logging in...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := cli.ChatClient.Login(ctx, req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.NotFound {
+			log.Printf("Invalid credentials: %v", err)
+		} else if ok && st.Code() == codes.Internal {
+			log.Fatalf("Server internal error: %v", err)
+		}
+		return
+	}
+
+	cli.Token = res.GetToken()
+	log.Print("Acquired new token: ", cli.Token)
+	return
 }
 
 func main() {
 	c := new(AccordClient)
 	c.Start()
+	c.CreateUser("testuser1", "testpw1")
+	c.Login("testuser1", "testpw1")
 }
--- server.go
+++ server.go
@@ -4,28 +4,97 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
+	"time"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	pb "github.com/qvntm/Accord/pb"
 )
 
-type AccordServer struct{}
+const (
+	secretKey     = "secret"
+	tokenDuration = 15 * time.Minute
+)
 
-func (s AccordServer) Login(_ context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+type AccordServer struct {
+	mutex      sync.RWMutex
+	users      map[string]*User
+	jwtManager *JWTManager
+}
+
+func NewAccordServer() *AccordServer {
+
+	return &AccordServer{
+		users:      make(map[string]*User),
+		jwtManager: NewJWTManager(secretKey, tokenDuration),
+	}
+}
+
+func (s *AccordServer) GetUser(username string) *User {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	user := s.users[username]
+	if user == nil {
+		return nil
+	}
+
+	return user.Clone()
+}
+
+func (s *AccordServer) CreateUser(_ context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	if s.users[req.GetUsername()] != nil {
+		return nil, status.Errorf(codes.AlreadyExists, "Username is already in use")
+	}
+
+	user, err := NewUser(req.GetUsername(), req.GetPassword(), "")
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Password could not be hashed")
+	}
+
+	s.users[user.Username] = user
+
+	res := &pb.CreateUserResponse{
+		Token: "somesortoftoken",
+	}
+	log.Printf("New user %s created", user.Username)
+	return res, nil
+}
+
+func (s *AccordServer) Login(_ context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	user := s.GetUser(req.GetUsername())
+
+	if user == nil {
+		return nil, status.Errorf(codes.NotFound, "incorrect username")
+	}
+	if !user.IsCorrectPassword(req.GetPassword()) {
+		return nil, status.Errorf(codes.NotFound, "incorrect password")
+	}
+
+	token, err := s.jwtManager.Generate(user)
+	if err != nil {
+		log.Print("token generation failed!")
+		return nil, status.Errorf(codes.Internal, "cannot generate access token")
+	}
+
+	res := &pb.LoginResponse{Token: token}
+	log.Printf("%s acquired new token", user.Username)
+	return res, nil
+}
+
+func (s *AccordServer) Logout(_ context.Context, req *pb.LogoutRequest) (*pb.LogoutResponse, error) {
 	return nil, fmt.Errorf("unimplemented!")
 }
 
-func (s AccordServer) Logout(_ context.Context, req *pb.LogoutRequest) (*pb.LogoutResponse, error) {
-	return nil, fmt.Errorf("unimplemented!")
-}
-
-func (s AccordServer) Stream(srv pb.Chat_StreamServer) error {
+func (s *AccordServer) Stream(srv pb.Chat_StreamServer) error {
 	return fmt.Errorf("unimplemented!")
 }
 
-func (s AccordServer) Start() error {
+func (s *AccordServer) Start() error {
 	fmt.Println("Starting up!")
 	listener, err := net.Listen("tcp", "0.0.0.0:12345")
 	if err != nil {
@@ -39,6 +108,6 @@ func (s AccordServer) Start() error {
 }
 
 func main() {
-	s := new(AccordServer)
+	s := NewAccordServer()
 	s.Start()
 }
