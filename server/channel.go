dc624e492af99a4ce7027bbfc3319666ccafdb90
temirrr <temirlanmirzakhmetov@gmail.com>
2020-08-16 07:10:19 +0900

    Clean up channel.go file and make the code compilable

--- server/channel.go
+++ server/channel.go
@@ -1,41 +1,57 @@
 package server
 
+// Message represents a single message in a channel.
 type Message struct {
 	timestamp uint64
 	from      string
 	content   string
 }
 
+// Permission type represents what user is permitted to do in the channel.
+type Permission string
+
+// WritePermission is a permission to write messages to the channel.
+var WritePermission Permission = "write_permission"
+
+// ReadPermission is a permission to read messages in the channel.
+var ReadPermission Permission = "read_permission"
+
+// Channel represents a single private or public messaging channel.
 type Channel struct {
-	channel_id          uint64
+	channelID           uint64
 	messages            []Message
 	msgc                chan Message
 	users               []User
-	subscription		[]
-	pinned_msg          uint64
-	is_public           bool
+	pinnedMsg           uint64
+	isPublic            bool
 	rolesWithPermission map[string][]string
 }
 
-func NewChannel(uid uint64, users []User, is_public bool) {
+// NewChannel creates a new channel with provided parameters.
+func NewChannel(uid uint64, users []User, isPublic bool) *Channel {
 	return &Channel{
-		channel_id: uid,
-		users:      users,
-		msgc:       make(chan Message),
-		is_public:  is_public,
+		channelID:           uid,
+		messages:            []Message{},
+		msgc:                make(chan Message),
+		users:               users,
+		pinnedMsg:           0,
+		isPublic:            isPublic,
+		rolesWithPermission: map[string][]string{},
 	}
 }
 
+// Listen listens for the incoming messages.
 func (ch *Channel) Listen() {
 	for {
 		select {
-		case msg := <-msgc:
-			ch.messages.append(msg)
+		case msg := <-ch.msgc:
+			ch.messages = append(ch.messages, msg)
 			ch.Broadcast(msg)
 		}
 	}
 }
 
+// Broadcast sends message to all users in the chat.
 func (ch *Channel) Broadcast(msg Message) {
 	return
 }
