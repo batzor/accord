369048f7645e1c62bbc741d0a04e666cde523464
temirrr <temirlanmirzakhmetov@gmail.com>
2020-08-19 22:13:00 +0900

    Define structs in the way I want them to look like
    By design decision:
    - package client will only hold list of channels
    - each package using package client, for example package cliapp,
    will store currentChannel and the mapping from channel IDs to
    related streams/channels. So the packages can stream with
    multiple streams at the same time if they want. Only if they
    want though; in our case, we will only stream with one channel
    at a time.

--- cliapp/app.go
+++ cliapp/app.go
@@ -5,17 +5,28 @@ import (
 
 	"github.com/jroimartin/gocui"
 	"github.com/qvntm/Accord/client"
-	"github.com/qvntm/Accord/pb"
 )
 
+type StreamCommunication struct {
+	reqComm client.StreamRequestCommunication
+	resComm client.StreamResponseCommunication
+}
+
 type ClientApp struct {
-	client       client.AccordClient
-	streamClient pb.Chat_StreamClient
+	client           client.AccordClient
+	currentChannelID uint64
+	// Currently, there will only be one channel-to-stream
+	// mapping at a time, because cliapp will only stream
+	// with one channel at a time, which is currentChannel.
+	// However, if there is need to stream with multiple
+	// channels simultaneously, then more keys can be added.
+	channelsToStreams map[uint64]StreamCommunication
 }
 
 func NewClientApp() *ClientApp {
+	serverID := uint64(67890) // dummy value so far
 	return &ClientApp{
-		client: *client.NewAccordClient(),
+		client: *client.NewAccordClient(serverID),
 	}
 }
 
