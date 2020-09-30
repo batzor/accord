7abc09531b2d5f3788d1eab83c346dd907a6c062
temirrr <temirlanmirzakhmetov@gmail.com>
2020-09-30 23:41:37 +0900

    Update sample.go

--- sample.go
+++ sample.go
@@ -16,7 +16,7 @@ var seededRand *rand.Rand = rand.New(
 	rand.NewSource(time.Now().UnixNano()),
 )
 
-func StringWithCharset(length int, charset string) string {
+func RandStringWithCharset(length int, charset string) string {
 	// for English letters, we can just use bytes instead
 	// of runes.
 	b := make([]byte, length)
@@ -26,20 +26,20 @@ func StringWithCharset(length int, charset string) string {
 	return string(b)
 }
 
-func String(length int) string {
-	return StringWithCharset(length, charset)
+func RandString(length int) string {
+	return RandStringWithCharset(length, charset)
 }
 
 func GetRandUsername() string {
-	return fmt.Sprintf("user_%s%s", StringWithCharset(3, letters), StringWithCharset(3, digits))
+	return fmt.Sprintf("user_%s%s", RandStringWithCharset(3, letters), RandStringWithCharset(3, digits))
 }
 
 func GetRandPassword() string {
-	return String(10)
+	return RandString(10)
 }
 
 func GetRandChannelName() string {
-	return fmt.Sprintf("chan_%s%s", StringWithCharset(3, letters), StringWithCharset(3, digits))
+	return fmt.Sprintf("chan_%s%s", RandStringWithCharset(3, letters), RandStringWithCharset(3, digits))
 }
 
 func GetRandBool() bool {
